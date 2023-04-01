cdac95c15dc78cda44cd23680345007f36b75c07
augustog <augusto.godoy@paydock.com>
2023-04-01 00:40:17 -0300

    Add auth + middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UsersModule } from "./users/users.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { MongooseModule } from "@nestjs/mongoose";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './controllers/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './controllers/auth/auth.module';
 
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forRoot({
-      type: "postgres",
+      type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/migrations/1650150312509-ArticleTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ArticleTable1650150312509 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
